1e4171a144cab1d054c9695dbe5e7f8f561fd85e
Saul Guaman <sguaman@baustro.fin.ec>
2023-07-06 15:31:31 -0500

    UpdateProject

---
+++ src/main/java/nl/amis/smeetsm/demoservice/SpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package nl.amis.smeetsm.demoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+
+}
